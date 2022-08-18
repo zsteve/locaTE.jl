30ca7b111032fc3b7f4de584d4851963ae98a5b4
Stephen Zhang <syz@syz.id.au>
2022-08-18 22:39:25 +1000

    update

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,4 @@
-symm(A::AbstractMatrix) = 0.5*(A + A')
+symm(A::AbstractMatrix) = max.(A, A') # 0.5*(A + A')
 symm(x::AbstractVector, n) = symm(reshape(x, n, n))
 symm_row(A::AbstractMatrix, n) = hcat([vec(symm(x, n)) for x in eachrow(A)]...)'
 cartesian_to_index(i, j; N) = N*(j-1)+i
