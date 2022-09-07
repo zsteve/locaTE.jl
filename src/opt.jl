3585df6517d463c6dfac27df33816dead301cdee
Stephen Zhang <stephenz@spartan-gpgpu084.hpc.unimelb.edu.au>
2022-09-07 11:10:03 +1000

    fix NTF code

--- src/opt.jl
+++ src/opt.jl
@@ -64,7 +64,7 @@ function fitsp_mean(G; ρ = 0.01, λ = 0.001, maxiter = 100)
     x
 end
 
-function fitnmf(G, L_all, L, k; α = 1e-2, λ = [1e-2, 1e-2], μ = [1e-2, 1e-2], iter = 500, print_iter = 50, initialize = :nndsvd, δ = 1e-5, dictionary = false, η = 1.0)
+function fitnmf(G, L_all, L, H, k; α = 1e-2, β = 0, λ = [1e-2, 1e-2], μ = [1e-2, 1e-2], iter = 500, print_iter = 50, initialize = :nndsvd, δ = 1e-5, dictionary = false, η = 1.0)
     # use multiplicative updates
     U = similar(G, size(G, 1), k)
     U_new = similar(U)
@@ -78,10 +78,10 @@ function fitnmf(G, L_all, L, k; α = 1e-2, λ = [1e-2, 1e-2], μ = [1e-2, 1e-2],
     A1 = D1 - L_all[1]
     A2 = D2 - L_all[2]
     # initialize
-    @info "Initializing as $(initialize)"
+    @info "Initializing NMF decomposition with $(initialize)"
     if initialize === :rand
-        U .= rand(size(U))
-        V .= rand(size(V))
+        copy!(U, rand(size(U)))
+        copy!(V, rand(size(V)))
     elseif initialize === :nndsvd
         U_init, V_init = NMF.nndsvd(G, k)
         copy!(U, U_init)
@@ -100,20 +100,17 @@ function fitnmf(G, L_all, L, k; α = 1e-2, λ = [1e-2, 1e-2], μ = [1e-2, 1e-2],
         function objective()
             Dict(:df => norm(U*V' - G, 2)^2 / 2,
                  :smooth => α/2*tr(V*U'*L*(U*V')),
+                 :affine => -β*tr(H'*U*V'), 
                  :smooth_U => λ[1]/2*tr(U'*L_all[1]*U),
                  :sp_U => λ[2]*norm(U, 1),
                  :smooth_V => μ[1]/2*tr(V'*L_all[2]*V),
                  :sp_V => μ[2]*norm(V, 1))
         end
-        # U_new .= relu.(U .* (G*V)) ./ (U*V'*V + λ[1]*(K1*U) .+ λ[2] .+ δ)
-        # U_new .= relu.(U .* (G*V + λ[1]*A1*U)) ./ (U*V'*V + λ[1]*(D1*U) .+ λ[2] .+ δ)
-        U_new .= relu.(U .* (G*V + λ[1]*A1*U + α*A*U*V'*V)) ./ (α*D*U*V'*V + U*V'*V + λ[1]*(D1*U) .+ λ[2] .+ δ)
+        U_new .= relu.(U .* (G*V + λ[1]*A1*U + α*A*U*V'*V + β*H*V)) ./ (α*D*U*V'*V + U*V'*V + λ[1]*(D1*U) .+ λ[2] .+ δ)
         if dictionary
             U_new ./= sum(U_new; dims = 2)
         end
-        # V_new .= relu.(V .* (G'*U)) ./ (V*U'*U + μ[1]*(K2*V) .+ μ[2] .+ δ)
-        # V_new .= relu.(V .* (G'*U + μ[1]*A2*V)) ./ (V*U'*U + μ[1]*(D2*V) .+ μ[2] .+ δ)
-        V_new .= relu.(V .* (G'*U + μ[1]*A2*V + α*V*U'*A*U)) ./ (α*V*U'*D*U + V*U'*U + μ[1]*(D2*V) .+ μ[2] .+ δ)
+        V_new .= relu.(V .* (G'*U + μ[1]*A2*V + α*V*U'*A*U + β*H'*U)) ./ (α*V*U'*D*U + V*U'*U + μ[1]*(D2*V) .+ μ[2] .+ δ)
         if (it % print_iter == 0)
             ΔU, ΔV = norm(U - U_new, Inf), norm(V - V_new, Inf)
             obj = objective()
@@ -130,47 +127,51 @@ function fitnmf(G, L_all, L, k; α = 1e-2, λ = [1e-2, 1e-2], μ = [1e-2, 1e-2],
     U, V, trace
 end
 
-function fitntf(G, L, λ, μ, α, k; iter = 250, print_iter = 50, dictionary = false, δ = 1e-5)
+function fitntf(G, L, L_g, λ, μ, α, k; iter = 250, print_iter = 50, dictionary = false, δ = 1e-5, η = 1.0)
+    # factor matrices
     A = [similar(G, size(G, i), k) for i = 1:length(size(G))]
     A_new = [similar(a) for a in A]
+    # core tensor
     S = zeros([k for _ = 1:length(size(G))]...)
     for i = 1:k
         S[i,i,i] = 1.0
     end
+    # initialization 
+    factor_cp = tl_decomp.non_negative_parafac(G, rank = k, init = "svd", n_iter_max = 1, random_state = 0)
     for i = 1:length(A)
-        factor_cp = tl_decomp.non_negative_parafac(G, rank = k, init = "svd", n_iter_max = 1, random_state = 0)
         copy!(A[i], factor_cp.factors[i])
     end
+    # normalize factor A^{(1)} if we need dictionary interpretation
     if dictionary
         A[1] ./= sum(A[1]; dims = 2)
     end
     # 
     D = [Diagonal(diag(l)) for l in L]
     W = [d - l  for (d, l) in zip(D, L)]
+    D_g = Diagonal(diag(L_g))
+    W_g = D_g - L_g
     ΔA = zeros(length(A))
     trace = []
     for it = 1:iter
         function objective()
             X = ttm(S, A, 1:length(A))
+            X1 = tenmat(X, 1)
             Dict(:df => norm(X - G, 2)^2 / 2,
-                    :smooth => [ λ[i]/2 * tr(A[i]'*L[i]*A[i]) for i = 1:length(A) ],
-                    :sparse => [ μ[i] * norm(A[i], 1) for i = 1:length(A) ]
+                 :smooth => [ λ[i]/2 * tr(A[i]'*L[i]*A[i]) for i = 1:length(A) ],
+                 :sparse => [ μ[i] * norm(A[i], 1) for i = 1:length(A) ],
+                 :smooth_mode1 => α/2*tr(X1'*L_g*X1)
                     )
         end
         for i = 1:length(A)
             inds = [j for j in range(length=length(A)) if j != i]
             Bi = tenmat(ttm(S, [Matrix(A[j]) for j in inds], inds), i)
-            # didn't split L into + and - parts 
-            # A_new[i] .= relu.(A[i] .* (tenmat(G, i) * Bi') ./ (A[i]*Bi*Bi' + λ[i]*L[i]*A[i] .+ μ[i] .+ δ))
-            # don't have full smoothness constraint 
-            # A_new[i] .= relu.(A[i] .* (tenmat(G, i) * Bi' + λ[i]*W[i]*A[i]) ./ (A[i]*Bi*Bi' + λ[i]*D[i]*A[i] .+ μ[i] .+ δ))
             if i == 1
-                A_new[i] .= relu.(A[i] .* (tenmat(G, i) * Bi' + λ[i]*W[i]*A[i] + α*W[i]*A[i]*Bi*Bi') ./ (A[i]*Bi*Bi' + λ[i]*D[i]*A[i] + α*D[i]*A[i]*Bi*Bi' .+ μ[i] .+ δ))
+                A_new[i] .= relu.(A[i] .* (tenmat(G, i) * Bi' + λ[i]*W[i]*A[i] + α*W_g*A[i]*Bi*Bi') ./ (A[i]*Bi*Bi' + λ[i]*D[i]*A[i] + α*D_g*A[i]*Bi*Bi' .+ μ[i] .+ δ))
             else
-                Btilde_plus = tenmat(ttm(S, [Matrix((j == 1 ? D[1] : I) * A[j]) for j in inds], inds), i)
-                Btilde_minus = tenmat(ttm(S, [Matrix((j == 1 ? W[1] : I) * A[j]) for j in inds], inds), i)
-                C_plus = Btilde_plus*Bi' + Bi*Btilde_plus'
-                C_minus = Btilde_minus*Bi' + Bi*Btilde_minus'
+                Btilde_plus = tenmat(ttm(S, [Matrix((j == 1 ? D_g : I) * A[j]) for j in inds], inds), i)
+                Btilde_minus = tenmat(ttm(S, [Matrix((j == 1 ? W_g : I) * A[j]) for j in inds], inds), i)
+                C_plus = (Btilde_plus*Bi' + Bi*Btilde_plus')/2
+                C_minus = (Btilde_minus*Bi' + Bi*Btilde_minus')/2
                 A_new[i] .= relu.(A[i] .* (tenmat(G, i) * Bi' + λ[i]*W[i]*A[i] + α*A[i]*C_minus) ./ (A[i]*Bi*Bi' + λ[i]*D[i]*A[i] + α*A[i]*C_plus .+ μ[i] .+ δ))
             end
             # normalize first factor
@@ -180,7 +181,8 @@ function fitntf(G, L, λ, μ, α, k; iter = 250, print_iter = 50, dictionary = f
             if it % print_iter == 1
                 ΔA[i] = norm(A[i] - A_new[i], Inf)
             end
-            copy!(A[i], A_new[i])
+            # copy!(A[i], A_new[i])
+            A[i] .= (1-η)*A[i] + η*A_new[i]
         end
         if it % print_iter == 1
             obj = objective()
